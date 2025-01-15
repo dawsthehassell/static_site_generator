d7fcc70a8e1a95e511b253116f336e47d28531e7
dawsthehassell <hasselldawson@gmail.com>
2025-01-15 14:46:28 -0600

    copy static to public in main.py, add public to gitignore

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
-from textnode import TextNode, TextType
+import shutil
+import os
+
+def file_copier(source="./static", destination="./public"):
+    source = os.path.abspath(source)
+    destination = os.path.abspath(destination)
+    file_list = os.listdir(source)
+    print(f"Current working directory: {os.getcwd()}")
+    print(f"Resolved destination path: {destination}")
+    if os.path.exists(destination):
+        if os.path.commonpath([os.getcwd(), os.path.abspath(destination)]) == os.getcwd():
+            shutil.rmtree(destination, ignore_errors=True)
+        else:
+            raise ValueError("Refusing to delete destination outside of project scope.")
+    shutil.rmtree(destination, ignore_errors=True)
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    
+    for item in file_list:
+        is_directory = os.path.exists(os.path.join(source, item)) and not os.path.isfile(os.path.join(source, item))
+        if is_directory:
+            subdirectory_path = os.path.join(destination, item)
+            if not os.path.exists(subdirectory_path):
+                os.mkdir(subdirectory_path)
+            file_copier(os.path.join(source, item), subdirectory_path)
+        else:
+            shutil.copy(os.path.join(source, item), os.path.join(destination, item))
 
 def main():
-    print("hello world")
+    source = "./static"
+    destination = "./public"
+    file_copier(source, destination)
+    print(f"Static files successfully copied from '{source}' to '{destination}'.")
 
 if __name__ == "__main__":
     main()
